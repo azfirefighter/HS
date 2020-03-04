66cf8d28f471f735d4f5686fa2e592605b111951
Stefan Saraev <stefan@saraev.ca>
2020-03-04 23:31:58 +0200

    [update] linux-ecb440abef

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="linux"
-PKG_VERSION="c54bc9cff3"
+PKG_VERSION="ecb440abef"
 PKG_SITE="https://github.com/raspberrypi/linux/tree/rpi-4.19.y"
 PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
