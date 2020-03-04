779ed69b032f1f7b816beebcc982e80ab5f73013
Stefan Saraev <stefan@saraev.ca>
2020-03-04 23:31:53 +0200

    [update] mmdvmhost-ab13f355d5

--- packages/base/mmdvmhost/package.mk
+++ packages/base/mmdvmhost/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="mmdvmhost"
-PKG_VERSION="8d87037ee5"
+PKG_VERSION="ab13f355d5"
 PKG_SITE="https://github.com/g4klx/MMDVMHost/tree/master"
 PKG_URL="https://github.com/g4klx/MMDVMHost/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="arduipi_oled i2c-tools"
