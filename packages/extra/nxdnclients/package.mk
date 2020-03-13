6b3cd0817db57e5d1a90efbc1f068409e9cb171e
Stefan Saraev <stefan@saraev.ca>
2020-03-13 19:39:30 +0200

    [update] nxdnclients-44ea6e7b68

--- packages/extra/nxdnclients/package.mk
+++ packages/extra/nxdnclients/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="nxdnclients"
-PKG_VERSION="77f55180af"
+PKG_VERSION="44ea6e7b68"
 PKG_SITE="https://github.com/g4klx/NXDNClients/tree/master"
 PKG_URL="https://github.com/g4klx/NXDNClients/archive/$PKG_VERSION.tar.gz"
 
